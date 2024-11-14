745f1367dd420a59a36375e9ba72bbb3339c2126
Vinith-Vj <vinithvj555@gmail.com>
2024-11-14 11:49:55 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function secondsToMinutesSeconds(seconds) {
 async function getSongs(folder) {
     currFolder = folder;
 
-    let a = await fetch(`https://raw.githubusercontent.com/Vinith-Vj/spotify-02/main/${folder}/`);
+    let a = await fetch(`/${folder}/`);
 
     let response = await a.text();
 
@@ -77,7 +77,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums () {
-    let a = await fetch(`https://raw.githubusercontent.com/Vinith-Vj/spotify-02/main/songs/`);
+    let a = await fetch(`/songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -89,8 +89,8 @@ async function displayAlbums () {
         const e = array[index];
 
         if (e.href.includes('/songs/')) {
-            let folder = e.href.split('/').slice(-1)[0]
-            let a = await fetch(`https://raw.githubusercontent.com/Vinith-Vj/spotify-02/main/${folder}/info.json`);
+            let folder = e.href.split('/').slice(-2)[0]
+            let a = await fetch(`/songs/${folder}/info.json`);
             let response = await a.json();
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
                         <div class="play">
